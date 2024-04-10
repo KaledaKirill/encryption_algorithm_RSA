2b2e5b031185428904d2ed4c3566912ac73851da
Kirill <kirill.kaleda16@gmail.com>
2024-04-10 14:39:03 +0300

    Write Read_message_from_file Func and Get_file_length Func

--- input_manager.c
+++ input_manager.c
@@ -6,6 +6,30 @@
 
 //all functions must return NULL if error is happend and print error message 
 
+FILE* open_file(const char* file_path, char* mode)
+{
+    FILE* file_ptr = fopen(file_path, mode);
+    if(file_ptr == NULL)
+    {
+        printf("\nFile opening error!\n");
+        return NULL;
+    }
+    return file_ptr;
+}
+
+long get_file_lenth(const char* file_path)
+{ 
+    FILE* file_ptr = open_file(file_path, "rb");
+    if(file_ptr == NULL) return 0;
+
+    fseek(file_ptr, 0, SEEK_END);
+    long file_size = ftell(file_ptr);
+
+    fclose(file_ptr);
+
+    return file_size;
+}
+
 int is_file_txt(const char *filepath) 
 {
     const char *extension = strrchr(filepath, '.');
@@ -18,23 +42,35 @@ int is_file_txt(const char *filepath)
 }
 
 
-char* read_message_from_file(char* file_path)
+char* read_message_from_file(const char* file_path)
 {
+    long file_length;
+    char* string;
+    char ch;
+
     if(is_file_txt(file_path) == 0)
     {
         printf("\nYour file does't have a .txt extension!\n");
         return NULL;
     }   
 
-    FILE* file_ptr = fopen(file_path, "r");
-    if(file_ptr == NULL)
+    FILE* file_ptr = open_file(file_path, "r");
+    if(file_ptr == NULL) return NULL;
+
+    file_length = get_file_lenth(file_path);
+    if(file_length == 0) return NULL;
+
+    string = (char*)malloc(file_length);
+    for(int i = 0; i < file_length; i++)
     {
-        printf("\nFile opening error!\n");
-        return NULL;
+        char ch = getc(file_ptr);
+        if(ch == '\n') ch = ' ';
+        string[i] = ch;
     }
 
-
     fclose(file_ptr);
+
+    return string;
 }
 
 
@@ -44,7 +80,15 @@ char* read_message_from_stdin()
 }
 
 
-void write_message_to_file(char* file_path, char* message)
+void write_message_to_file(const char* file_path, char* message)
 {
 
+}
+
+int main(void)
+{
+    char* string = read_message_from_file("data.txt");
+    printf("\n%s\n", string);
+
+    return 0;
 }
